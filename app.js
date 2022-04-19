4677ac3c1d42982ea6b95a2a885658e32579d585
Bloodiko <bastianjesuiter@gmail.com>
2022-04-19 15:52:09 +0200

    base electron window and launch

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
-// run application in loop
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+console.log(app);
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
 
 
 
