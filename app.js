fe15f37ebeca2b900582ba5cf47b71c59f52961d
Bloodiko <bastianjesuiter@gmail.com>
2022-04-19 11:09:29 +0200

    base commit for shadio

---
+++ app.js
@@ -0,0 +1,20 @@
+// loop application
+var app = {};
+
+/*
+// Permanently record desktop audio from the default input device.
+const recorder = require('node-record-lpcm16');
+const fs = require('fs');
+
+// create global hotkey with control + alt + printscreen
+const { registerHotkey } = require('node-hotkey');
+registerHotkey('control+alt+printscreen', () => {
+    // start recording
+    recorder.start().pipe(fs.createWriteStream('output.raw'));
+    // stop recording after 5 seconds
+    setTimeout(() => {
+        recorder.stop();
+    }, 5000);
+});
+
+*/
